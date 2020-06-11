db0b4b898709fd78d4a1c2296db764b973cf507d
ajh <ajh7893@gmail.com>
2020-06-11 22:40:29 +0900

    react-status

--- src/App.js
+++ src/App.js
@@ -1,61 +1,25 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import PropTypes from 'prop-types';
 
+class App extends React.Component{
+  state ={
+    count: 0,
+  };  
 
-function Food({ name, picture, rating }) {
+  add = () => {
+    this.setState({count: this.state.count + 1});
+  };
+  minus = () =>{
+    this.setState({count: this.state.count -1});
+  };
 
-    return (<div >
-        <h2> I like {name} </h2>
-        <h4 > {rating} / 5.0 </h4>
-        <img src={picture} alt={name} />
-    </div>
-    );
-}
-
-const foodILike = [
-
-    {
-        id: 1,
-        name: 'kimchi',
-        image: 'http://egnmall.net/data/rental/goods/big/201921237024768.jpg',
-        rating: 5,
-    },
-    {
-        id: 2,
-        name: 'doncasu',
-        image: 'http://th2.tmon.kr/thumbs/image/423/336/e17/1b1492318_700x700_95_FIT.jpg',
-        rating: 4.9,
-    },
-    {
-        id: 3,
-        name: 'bibibob',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/440px-Dolsot-bibimbap.jpg',
-        rating: 2,
-    },
-];
-
-
-function App() {
-
-    return (
+  render(){
+      return (
         <div>
-
-            {
-                foodILike.map(dish => (
-                    <Food key={dish.id}
-                        name={dish.name}
-                        picture={dish.image}
-                        rating={dish.rating} />
-                ))
-            };
+          <h1>the number is: {this.state.count}</h1>
+          <button onClick={this.add}>Add</button>
+          <button onClick={this.minus}>Minus</button>
         </div>
-    );
+        );
+    }
 }
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating: PropTypes.number,
-};
-
 export default App;
