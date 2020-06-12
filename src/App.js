a86de553f796336dabe78048d755175d1bf52348
ajh <ajh7893@gmail.com>
2020-06-12 16:13:39 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 
 class App extends React.Component{
+  
   state ={
-    count: 0,
-  };  
-
-  add = () => {
-    this.setState({count: this.state.count + 1});
+    isLoading: true,
   };
-  minus = () =>{
-    this.setState({count: this.state.count -1});
-  };
-
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
+  }
   render(){
-      return (
-        <div>
-          <h1>the number is: {this.state.count}</h1>
-          <button onClick={this.add}>Add</button>
-          <button onClick={this.minus}>Minus</button>
-        </div>
-        );
-    }
+    const {isLoading} = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>
+  }
 }
 export default App;
